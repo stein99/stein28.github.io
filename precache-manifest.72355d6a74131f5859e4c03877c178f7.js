dd296a9958056edc4eca4beacc9a754d47829af8
= <=>
2019-11-19 09:26:05 +0700

    my first commit

---
+++ precache-manifest.72355d6a74131f5859e4c03877c178f7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddd470e2a5820e7f60e7",
+    "url": "/css/about.51df41b4.css"
+  },
+  {
+    "revision": "efd8e0a45b4d215c688f",
+    "url": "/css/chunk-vendors.70084ded.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "843ca507c1c8124a44c862e232d086f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddd470e2a5820e7f60e7",
+    "url": "/js/about.1172a483.js"
+  },
+  {
+    "revision": "9d7419151326d29f6e3b",
+    "url": "/js/app.fb1f7509.js"
+  },
+  {
+    "revision": "efd8e0a45b4d215c688f",
+    "url": "/js/chunk-vendors.64e5649b.js"
+  },
+  {
+    "revision": "099f6fc59305d3c56e80993124a13412",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
